97a4e7a1de7fda1228584dc015cd8f64f20934be
Noel Cower <ncower@gmail.com>
2013-07-04 17:09:35 -0700

    Note about thread-safety of have_gl_command? and load_all_gl_commands!
    Not really necessary, given that this is loading commands at runtime,
    but Ruby folks may not be as conscious of what counts as thread-safe and
    so on. So, here's the deal: if a Gl command is being loaded, one cannot
    call have_gl_command? or otherwise call an unloaded function from
    another thread. This is because there is no mutex around the Gl function
    Hash and so on.
    
    If you want some amount of thread-safety, call load_all_gl_commands!
    before any threads are spawned. This will ensure that no Gl commands are
    loaded in the future unless you do something weird like modify
    GL_COMMAND_TYPES and add a new command, which _you should not do_.

--- lib/opengl-core.rb
+++ lib/opengl-core.rb
@@ -5,10 +5,19 @@ require 'opengl-core/gl_commands'
 
 module Gl
 
+  # Checks if a GL command is availalbe to use. This necessarily loads the
+  # command if it's not yet loaded just to check if it exists, so do not call
+  # this from multiple threads when other Gl commands are being loaded. If you
+  # want to ensure this only reads, you can call load_all_gl_commands! ahead of
+  # time and query it afterward.
   def have_gl_command?(command)
     !!GlSym.__load_gl_sym__(command.intern)
   end
 
+  # Does what it says on the tin. Should only be called once, preferably from
+  # the main thread, though I'm not aware of any thread requirements re: symbol
+  # loading. If you're using Gl commands from multiple threads with multiple
+  # contexts, you should call this before using any Gl commands.
   def load_all_gl_commands!()
     GlSym::GL_COMMAND_TYPES.each_key { |fnsym| GlSym.__load_gl_sym__(fnsym) }
     self
